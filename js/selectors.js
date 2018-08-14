86fcde8ab3fcce655973da2c30bddb54060f4084
Caitlin Lauer <caitlinrinn@gmail.com>
2018-08-14 14:12:31 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,7 +5,7 @@ function paragraphSelector() {
 }
 
 function lastImageSelector() {
-  var image = document.getElementsByTagName("img").last();
+  var image = document.getElementsByTagName("div").last();
   return image;
   
 }
